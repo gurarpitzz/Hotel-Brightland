531e69095c80d6549fc302fa6d768898458b6b59
gurarpitzz <gurarpitzz@github.com>
2026-08-21 18:08:18 +0530

    Use 177.77vh x 56.25vw 16:9 aspect fill formula for YouTube iframe background to eliminate black borders

--- src/components/HeroVideoSection.tsx
+++ src/components/HeroVideoSection.tsx
@@ -120,8 +120,11 @@ export default function HeroVideoSection({
       >
         {/* YouTube Background Embed with Aspect Ratio Scaling */}
         <div className="absolute inset-0 pointer-events-none overflow-hidden flex items-center justify-center">
-          <div className="relative w-[450%] h-[250%] sm:w-[180%] sm:h-[180%] lg:w-[125%] lg:h-[125%] min-w-full min-h-full flex items-center justify-center">
-            <div id="hero-yt-player" className="w-full h-full scale-[1.6] sm:scale-100 transform origin-center" />
+          <div className="relative w-[300vw] h-[300vh] sm:w-[180%] sm:h-[180%] lg:w-[125%] lg:h-[125%] min-w-full min-h-full flex items-center justify-center">
+            <iframe
+              id="hero-yt-player"
+              className="w-[177.77vh] min-w-full h-[56.25vw] min-h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover pointer-events-none"
+            />
           </div>
         </div>
 
