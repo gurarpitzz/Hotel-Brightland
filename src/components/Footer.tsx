51ab90de3cfae44c3ba523b080e24dfd7216c2fe
gurarpitzz <gurarpitzz@github.com>
2026-08-26 14:52:12 +0530

    Update footer transition wave to a prominent, deep organic SVG curve

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,10 +30,10 @@ export default function Footer() {
 
   return (
     <footer className="relative bg-brand-green-900 text-brand-yellow-50 pt-16 pb-12 mt-auto">
-      {/* Sleek Low-Profile Elegant Curved Wave */}
+      {/* Prominent Deep Organic Wave Divider */}
       <div className="absolute top-0 left-0 w-full overflow-hidden leading-none z-20 pointer-events-none -translate-y-[99%]">
-        <svg className="relative block w-full h-[25px] md:h-[42px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100" preserveAspectRatio="none">
-          <path d="M0,32 C360,95 720,5 1080,65 C1260,90 1380,50 1440,32 L1440,100 L0,100 Z" fill="#022c22"></path>
+        <svg className="relative block w-full h-[60px] md:h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" preserveAspectRatio="none">
+          <path d="M0,45 C280,125 560,5 840,90 C1120,135 1300,40 1440,25 L1440,120 L0,120 Z" fill="#022c22"></path>
         </svg>
       </div>
 
