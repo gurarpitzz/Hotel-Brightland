2533fc1948c10fefcf62a8181263a7765d35e827
gurarpitzz <gurarpitzz@github.com>
2026-08-27 12:33:49 +0530

    Install Google Tag Manager container GTM-NW83K79X in head and body of layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -194,6 +194,18 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${greatVibes.variable} ${cormorantGaramond.variable} ${plusJakartaSans.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NW83K79X');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <link rel="icon" href="/assets/logo/logo.png" type="image/png" />
         <link rel="shortcut icon" href="/assets/logo/logo.png" type="image/png" />
         <link rel="apple-touch-icon" href="/assets/logo/logo.png" />
@@ -239,6 +251,17 @@ export default function RootLayout({
         )}
       </head>
       <body className="min-h-full flex flex-col pb-16 md:pb-0">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NW83K79X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Providers>
           <SplashLoader />
           <Header />
